fca8201e85f49a3d96e9d685240092c6ed0ae635
yongfeng <yongfeng_gu@163.com>
2018-11-28 16:02:38 +0800

    add comments on TwiseSampling.java

--- src/core/algorithms/TwiseSampling.java
+++ src/core/algorithms/TwiseSampling.java
@@ -11,14 +11,23 @@ import java.util.List;
 
 import core.SamplingAlgorithm;
 
+/**
+ * <p><b>TwiseSampling T 覆盖采样</b> 方法采用贪心策略选取能够满足覆盖任意 T 个配置项的最小采样空间。</p>
+ * @author yongfeng
+ */
 public class TwiseSampling extends SamplingAlgorithm {
-
+	/** T-wise 的 T 值 */
 	private int t;
-	
-	public TwiseSampling(int t) {
+	/** TwsieSampling 的构造函数， 初始化 T 值。 */
+	public TwiseSampling(int t) { // 初始化 T 值
 		this.t = t;
 	}
 	
+	/**
+	 * <p>利用 T 覆盖采样的方法抽取 file 文件中的配置，采样个数即配置项的个数不定。</p>
+	 * <p>由于事先已经使用了 SPLCAT 工具算好了 T-wise 下的覆盖矩阵 (存放在根目录下的 tables 文件夹下)，因此只需要匹配相应的 配置项个 数和 T 值即可匹配到正确的覆盖矩阵。</p>
+	 * @param file C 文件
+	 */
 	@Override
 	public List<List<String>> getSamples(File file) throws Exception {
 		List<List<String>> configurations = new ArrayList<>();
