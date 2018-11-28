0ed25f4d0079b8580fd10e205e501e67aa6d9b55
yongfeng <yongfeng_gu@163.com>
2018-11-28 15:50:42 +0800

    add comments on AllEnabledDisabledSampling.java

--- src/core/algorithms/AllEnabledDisabledSampling.java
+++ src/core/algorithms/AllEnabledDisabledSampling.java
@@ -6,8 +6,16 @@ import java.util.List;
 
 import core.SamplingAlgorithm;
 
+/**
+ * <p><b>AllEnabledDisabledSampling 全启禁采样</b> 方法抽取 2 个配置样本，第 1 个样本精通全部配置项，第 2 个样本启用全部配置项。</p>
+ * @author yongfeng
+ */
 public class AllEnabledDisabledSampling extends SamplingAlgorithm{
 
+	/**
+	 * <p>利用全启禁采样的方法抽取 file 文件中的配置，采样个数为 2。</p>
+	 * @param file C 文件
+	 */
 	@Override
 	public List<List<String>> getSamples(File file) throws Exception {
 		List<List<String>> configurations = new ArrayList<>();
@@ -18,7 +26,7 @@ public class AllEnabledDisabledSampling extends SamplingAlgorithm{
 			// All macros activated..
 			List<String> allActive = new ArrayList<String>();
 			for (String d : directives){
-				allActive.add(d);
+				allActive.add(d); // 启用全部配置项
 			}
 			configurations.add(allActive);
 		
@@ -26,7 +34,7 @@ public class AllEnabledDisabledSampling extends SamplingAlgorithm{
 			// All macros activated..
 			List<String> allDeactive = new ArrayList<String>();
 			for (String d : directives){
-				allDeactive.add("!" + d);
+				allDeactive.add("!" + d); // 禁用全部配置项
 			}
 			configurations.add(allDeactive);
 		
